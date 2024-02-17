e0014f4ddfe351e078c456863f2926dc5c7283a8
NahumR <xnahumtec@gmail.com>
2024-02-17 14:43:39 -0600

    creacion de componente navbar

---
+++ src/components/ContactComponent.vue
@@ -0,0 +1,24 @@
+<template>
+    <div>
+        <nav>
+            <li>
+                Home
+            </li>
+            <li>
+                Experience
+            </li>
+            <li>
+                Contact
+            </li>
+        </nav>
+    </div>
+
+</template>
+
+<script>
+
+</script>
+
+<style>
+
+</style>
